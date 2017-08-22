433a10248b7fede7ecdb0e3278218629117b2110
Anuj Gupta <junagupta19@gmail.com>
2017-08-22 14:19:11 +0530

    Added Click listener on bottom navigation view

--- app/src/main/java/in/androidmate/anujgupta/bottomnavigationbar/MainActivity.java
+++ app/src/main/java/in/androidmate/anujgupta/bottomnavigationbar/MainActivity.java
@@ -1,7 +1,12 @@
 package in.androidmate.anujgupta.bottomnavigationbar;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_favorites:
+                                Toast.makeText(MainActivity.this,"Favorites" ,Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.action_schedules:
+                                Toast.makeText(MainActivity.this,"Schedules",Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.action_music:
+                                Toast.makeText(MainActivity.this,"Music",Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                        return false;
+                    }
+                });
     }
 }
