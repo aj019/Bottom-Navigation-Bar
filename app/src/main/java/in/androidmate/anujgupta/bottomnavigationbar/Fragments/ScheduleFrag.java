7c5b2d664d4403bd150c8c6e2eb6f5584ab1c3d7
Anuj Gupta <junagupta19@gmail.com>
2017-08-22 15:03:11 +0530

    Added fragments

---
+++ app/src/main/java/in/androidmate/anujgupta/bottomnavigationbar/Fragments/ScheduleFrag.java
@@ -0,0 +1,21 @@
+package in.androidmate.anujgupta.bottomnavigationbar.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by anujgupta on 22/08/17.
+ */
+
+public class ScheduleFrag extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
